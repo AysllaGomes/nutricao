eaf6d2ab7b5058c8ae40a3c136924ef2cd3ac6b6
ayslla.gomes <ayslla.gomes@outlook.com>
2021-02-02 08:05:54 -0300

    [UPT] [ARQUITETURA] - Instalando o theme, seguindo a documentação do Material Angular

--- codigo-fonte/cliente/src/app/app.module.ts
+++ codigo-fonte/cliente/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        RouterModule,
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
